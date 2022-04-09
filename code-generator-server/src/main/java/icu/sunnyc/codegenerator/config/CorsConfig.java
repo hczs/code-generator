578de53e466cfb60861b0de2c3a59a2dee3b3d04
houcheng <houcheng@boco.com.cn>
2022-04-09 23:07:59 +0800

    :wrench: 添加跨域配置

--- code-generator-server/src/main/java/icu/sunnyc/codegenerator/config/CorsConfig.java
+++ code-generator-server/src/main/java/icu/sunnyc/codegenerator/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package icu.sunnyc.codegenerator.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -8,6 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date ：Created in 2022/4/9 22:53
  * @modified ：
  */
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
 
 }
