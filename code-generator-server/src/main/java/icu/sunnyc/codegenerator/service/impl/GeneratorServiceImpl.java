a36d67560b68c885f3d7644b66ff13d36514f6ab
houcheng <houcheng@boco.com.cn>
2022-04-11 11:09:05 +0800

    :hankey: 完善代码

--- code-generator-server/src/main/java/icu/sunnyc/codegenerator/service/impl/GeneratorServiceImpl.java
+++ code-generator-server/src/main/java/icu/sunnyc/codegenerator/service/impl/GeneratorServiceImpl.java
@@ -1,13 +1,15 @@
 package icu.sunnyc.codegenerator.service.impl;
 
+import icu.sunnyc.codegenerator.exception.CodeGenerateException;
 import icu.sunnyc.codegenerator.service.GeneratorService;
 import icu.sunnyc.codegenerator.utils.FreemarkerUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * @author ：hc
@@ -15,6 +17,7 @@ import java.util.Map;
  * @modified ：
  */
 @Service
+@Slf4j
 public class GeneratorServiceImpl implements GeneratorService {
 
     @Override
@@ -22,7 +25,6 @@ public class GeneratorServiceImpl implements GeneratorService {
         HashMap<String, String> resultMap = new HashMap<>();
         List<String> allTemplate = getAllTemplate();
         for (String template : allTemplate) {
-            params.put("hello", "hello, world!");
             String result = FreemarkerUtil.renderAndGetString(template, params);
             resultMap.put(template.split("\\.")[0], result);
         }
@@ -33,15 +35,15 @@ public class GeneratorServiceImpl implements GeneratorService {
     public List<String> getAllTemplate() {
         // 后续数据库存储模板信息 可自定义上传模板相关操作
         ArrayList<String> templates = new ArrayList<>();
-        templates.add("entity.ftl");
-        templates.add("mapper.ftl");
-        templates.add("service.ftl");
-        templates.add("service_impl.ftl");
-        templates.add("controller.ftl");
-        templates.add("select.ftl");
-        templates.add("index.ftl");
-        templates.add("api.ftl");
-        return templates;
+        try {
+            File dir = ResourceUtils.getFile("classpath:templates");
+            File[] files = dir.listFiles();
+            assert files != null;
+            Arrays.stream(files).forEach(file -> templates.add(file.getName()));
+            return templates;
+        } catch (FileNotFoundException e) {
+            log.error("模板文件未找到：{}", e.getMessage(), e);
+            throw new CodeGenerateException("模板文件未找到");
+        }
     }
-
 }
--- code-generator-server/src/test/java/icu/sunnyc/codegenerator/CodeGeneratorApplicationTests.java
+++ code-generator-server/src/test/java/icu/sunnyc/codegenerator/CodeGeneratorApplicationTests.java
@@ -5,11 +5,13 @@ import icu.sunnyc.codegenerator.entity.ParamInfo;
 import icu.sunnyc.codegenerator.service.GeneratorService;
 import icu.sunnyc.codegenerator.utils.FreemarkerUtil;
 import icu.sunnyc.codegenerator.utils.TableParseUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
@@ -18,64 +20,28 @@ class CodeGeneratorApplicationTests {
     @Autowired
     private GeneratorService generatorService;
 
-    @Test
-    void contextLoads() {
-    }
-
     /**
      * 测试解析 DDL SQL
      */
     @Test
-    public void testGetClassInfo() {
+    void testGetClassInfo() {
         ParamInfo paramInfo = new ParamInfo();
-        paramInfo.setTableSql("-- IDCDB.T_NS_MESSAGE_POLICY definition\n" +
-                "\n" +
-                "CREATE TABLE \"IDCDB\".\"T_NS_MESSAGE_POLICY\" \n" +
-                "   (\t\"POLICY_ID\" VARCHAR2(32) NOT NULL ENABLE, \n" +
-                "\t\"POLICY_NAME\" VARCHAR2(128) NOT NULL ENABLE, \n" +
-                "\t\"FLOW_TYPE\" NUMBER NOT NULL ENABLE, \n" +
-                "\t\"START_TIME\" DATE NOT NULL ENABLE, \n" +
-                "\t\"END_TIME\" DATE NOT NULL ENABLE, \n" +
-                "\t\"DEL_FLAG\" CHAR(1) DEFAULT 0 NOT NULL ENABLE, \n" +
-                "\t\"CREATE_BY\" VARCHAR2(64) NOT NULL ENABLE, \n" +
-                "\t\"CREATE_TIME\" DATE NOT NULL ENABLE, \n" +
-                "\t\"UPDATE_BY\" VARCHAR2(64), \n" +
-                "\t\"UPDATE_TIME\" DATE, \n" +
-                "\t\"RULE_TYPE\" NUMBER NOT NULL ENABLE, \n" +
-                "\t\"EVENT_TYPE\" VARCHAR2(5) NOT NULL ENABLE, \n" +
-                "\t\"RULE_DESCRIPTION\" VARCHAR2(128) NOT NULL ENABLE, \n" +
-                "\t\"SNORT_RULE\" VARCHAR2(128), \n" +
-                "\t\"MTX_RULE\" VARCHAR2(128), \n" +
-                "\t\"IOC_RULE\" VARCHAR2(128), \n" +
-                "\t\"IS_PCAP\" NUMBER NOT NULL ENABLE, \n" +
-                "\t\"VERSION\" VARCHAR2(8) NOT NULL ENABLE\n" +
-                "   ) SEGMENT CREATION IMMEDIATE \n" +
-                "  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 \n" +
-                " NOCOMPRESS LOGGING\n" +
-                "  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645\n" +
-                "  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1\n" +
-                "  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)\n" +
-                "  TABLESPACE \"IDCDBS\" ;\n" +
-                "\n" +
-                "COMMENT ON TABLE IDCDB.T_NS_MESSAGE_POLICY IS '恶意报文检测指令策略表';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.POLICY_ID IS '策略号';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.POLICY_NAME IS '策略名称';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.FLOW_TYPE IS '流量类型（1 入向 2 出向 3 双向）';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.START_TIME IS '策略生效时间';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.END_TIME IS '策略过期时间';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.DEL_FLAG IS '删除标志（0 未删除 2 已删除）';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.CREATE_BY IS '创建者';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.CREATE_TIME IS '创建时间';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.UPDATE_BY IS '更新者';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.UPDATE_TIME IS '更新时间';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.RULE_TYPE IS '规则类型（1 snort规则 2 mtx规则 3 威胁情报-IP规则 4 威胁情报-URL规则 5 威胁情报-域名规则）';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.EVENT_TYPE IS '事件分类（存储五位事件编号）';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.RULE_DESCRIPTION IS '规则描述';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.SNORT_RULE IS 'Snort 规则';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.MTX_RULE IS 'MTX规则';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.IOC_RULE IS '威胁情报规则';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.IS_PCAP IS '是否带pcap上报（0 不带pcap 1 带pcap）';\n" +
-                "COMMENT ON COLUMN IDCDB.T_NS_MESSAGE_POLICY.VERSION IS '版本号';");
+        paramInfo.setTableSql("CREATE TABLE 't_staff' (\n" +
+                "  'id' char(19) NOT NULL,\n" +
+                "  'username' varchar(10) NOT NULL COMMENT '用户名',\n" +
+                "  'password' varchar(300) NOT NULL COMMENT '密码',\n" +
+                "  'mail' varchar(50) NOT NULL COMMENT '邮箱',\n" +
+                "  'gender' tinyint(4) NOT NULL DEFAULT '1' COMMENT '性别 1男，2女',\n" +
+                "  'avatar' varchar(300) DEFAULT NULL COMMENT '用户头像',\n" +
+                "  'introduce' varchar(100) DEFAULT NULL COMMENT '个人介绍',\n" +
+                "  'permission' int(11) NOT NULL DEFAULT '0' COMMENT '权限，0是普通用户，1是管理员',\n" +
+                "  'del_flag' tinyint(4) NOT NULL DEFAULT '0' COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',\n" +
+                "  'create_time' datetime NOT NULL COMMENT '创建时间',\n" +
+                "  'update_time' datetime NOT NULL COMMENT '更新时间',\n" +
+                "  PRIMARY KEY ('id'),\n" +
+                "  UNIQUE KEY 'username' ('username') USING BTREE,\n" +
+                "  UNIQUE KEY 'mail' ('mail')\n" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
         // 设置表格参数
         HashMap<String, Object> options = new HashMap<>();
         paramInfo.setOptions(options);
@@ -89,6 +55,7 @@ class CodeGeneratorApplicationTests {
         Map<String, String> map = generatorService.renderTemplate(paramInfo.getOptions());
         System.out.println(classInfo);
         System.out.println(map);
+        Assertions.assertTrue(map.size() > 0);
     }
 
 
@@ -96,21 +63,30 @@ class CodeGeneratorApplicationTests {
      * 测试模板渲染
      */
     @Test
-    public void testFreemarker() {
+    void testFreemarker() {
         HashMap<String, Object> params = new HashMap<>();
         params.put("hello", "hello world!");
-        System.out.println(FreemarkerUtil.renderAndGetString("entity.ftl", params));
+        String result = FreemarkerUtil.renderAndGetString("entity.ftl", params);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void testGetAllTemplates() {
+        List<String> allTemplate = generatorService.getAllTemplate();
+        System.out.println(allTemplate);
+        Assertions.assertTrue(allTemplate.size() > 0);
     }
 
     /**
      * 测试模板渲染业务接口
      */
     @Test
-    public void testRenderTemplate() {
+    void testRenderTemplate() {
         HashMap<String, Object> params = new HashMap<>();
         params.put("hello", "hello world!");
         Map<String, String> map = generatorService.renderTemplate(params);
         System.out.println(map);
+        Assertions.assertTrue(map.size() > 0);
     }
 
 }
