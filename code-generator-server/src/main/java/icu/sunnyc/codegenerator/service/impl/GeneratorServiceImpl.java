f064967c3fe80e325d240a452b46a9d1210c76c4
houcheng <houcheng@boco.com.cn>
2022-04-11 11:41:17 +0800

    :bug: 读取模板文件暂时恢复硬编码方式

--- code-generator-server/src/main/java/icu/sunnyc/codegenerator/service/impl/GeneratorServiceImpl.java
+++ code-generator-server/src/main/java/icu/sunnyc/codegenerator/service/impl/GeneratorServiceImpl.java
@@ -37,16 +37,15 @@ public class GeneratorServiceImpl implements GeneratorService {
     public List<String> getAllTemplate() {
         // 后续数据库存储模板信息 可自定义上传模板相关操作
         ArrayList<String> templates = new ArrayList<>();
-        try {
-            File dir = new ClassPathResource("templates").getFile();
-            File[] files = dir.listFiles();
-            assert files != null;
-            Arrays.stream(files).forEach(file -> templates.add(file.getName()));
-            return templates;
-        } catch (IOException e) {
-            log.error("模板文件未找到：{}", e.getMessage(), e);
-            throw new CodeGenerateException("模板文件未找到");
-        }
-
+        // 暂时恢复硬编码方式 后续有时间再改
+        templates.add("entity.ftl");
+        templates.add("mapper.ftl");
+        templates.add("service.ftl");
+        templates.add("service_impl.ftl");
+        templates.add("controller.ftl");
+        templates.add("select.ftl");
+        templates.add("index.ftl");
+        templates.add("api.ftl");
+        return templates;
     }
 }
